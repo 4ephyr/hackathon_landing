3bb31b36ca327f1d0efa2af5b9d4c9000b97bc96
Mohammed Sahal <technicalsahal25@gmail.com>
2025-02-08 22:17:28 +0530

    Updated

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -6,7 +6,7 @@ export default function Header() {
         <div className="flex justify-center items-center border-y-4 border-black my-4">
           <div className="relative ">
               <h1 className="font-secondary text-6xl p-5">TakeDown</h1>
-              <Image className="absolute top-[-24px] right-[5px]" src="/assets/curl4.svg" alt="Curvy Line" height={100} width={100} />
+              <Image className="absolute top-[-24px] right-[5px]" src="assets/curl4.svg" alt="Curvy Line" height={100} width={100} />
           </div>
         </div>
     </div>
--- src/components/Loader.tsx
+++ src/components/Loader.tsx
@@ -9,7 +9,7 @@ export default function Loader() {
           initial={{ y: 0 }}
           animate={{ y: "-110%" }}
           transition={{ duration: 1.5 }}
-          className="absolute top-0 left-0 w-full h-1/2 bg-secondary bg-[url('/natural-paper.png')] bg-repeat text-gray-900 before:content-[''] before:absolute before:inset-0 before:bg-[radial-gradient(circle,rgba(0,0,0,0.15)_10%,transparent_50%),radial-gradient(circle_at_20%_80%,rgba(0,0,0,0.1)_10%,transparent_50%),radial-gradient(circle_at_80%_20%,rgba(0,0,0,0.1)_10%,transparent_50%)] before:pointer-events-none before:z-10 shadow-loader_top"
+          className="absolute top-0 left-0 w-full h-1/2 bg-secondary bg-[url('natural-paper.png')] bg-repeat text-gray-900 before:content-[''] before:absolute before:inset-0 before:bg-[radial-gradient(circle,rgba(0,0,0,0.15)_10%,transparent_50%),radial-gradient(circle_at_20%_80%,rgba(0,0,0,0.1)_10%,transparent_50%),radial-gradient(circle_at_80%_20%,rgba(0,0,0,0.1)_10%,transparent_50%)] before:pointer-events-none before:z-10 shadow-loader_top"
         >
         </motion.div>
 
@@ -17,7 +17,7 @@ export default function Loader() {
           initial={{ y: 0 }}
           animate={{ y: "110%" }}
           transition={{ duration: 1.5 }}
-          className="absolute top-1/2 left-0 w-full h-1/2 bg-secondary bg-[url('/natural-paper.png')] bg-repeat text-gray-900 before:content-[''] before:absolute before:inset-0 before:bg-[radial-gradient(circle,rgba(0,0,0,0.15)_10%,transparent_50%),radial-gradient(circle_at_20%_80%,rgba(0,0,0,0.1)_10%,transparent_50%),radial-gradient(circle_at_80%_20%,rgba(0,0,0,0.1)_10%,transparent_50%)] before:pointer-events-none before:z-10 shadow-loader_bot"
+          className="absolute top-1/2 left-0 w-full h-1/2 bg-secondary bg-[url('natural-paper.png')] bg-repeat text-gray-900 before:content-[''] before:absolute before:inset-0 before:bg-[radial-gradient(circle,rgba(0,0,0,0.15)_10%,transparent_50%),radial-gradient(circle_at_20%_80%,rgba(0,0,0,0.1)_10%,transparent_50%),radial-gradient(circle_at_80%_20%,rgba(0,0,0,0.1)_10%,transparent_50%)] before:pointer-events-none before:z-10 shadow-loader_bot"
         >
         </motion.div>
       </div>
