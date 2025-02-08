ee98b45b414def560a90cab05cf83f0bdd8f43fd
Mohammed Sahal <technicalsahal25@gmail.com>
2025-02-09 00:16:09 +0530

    Updated

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,7 +3,7 @@ import Header from "@/components/Header";
 
 export default function Home() {
   return (
-    <div className="container mx-auto">
+    <div className="o">
       <Loader />
       <Header />
     </div>
--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -3,13 +3,13 @@ import Image from 'next/image'
 export default function Header() {
   return (
     <div>
-        <div className="flex justify-between items-center border-y-4 border-primary my-4">
-          <Image src="/hackathon_landing/assets/curl1.svg" alt="Logo" height={100} width={100} />
+        <div className="relative flex justify-center items-center border-y-4 border-primary my-4">
+          <Image className='absolute left-2' src="/hackathon_landing/assets/curl1.svg" alt="Logo" height={150} width={150} />
           <div className="relative ">
               <h1 className="font-secondary text-6xl p-5">TakeDown</h1>
               <Image className="absolute top-[-24px] right-[5px]" src="/hackathon_landing/assets/curl4.svg" alt="Curvy Line" height={100} width={100} />
           </div>
-          <Image className='rotate-180' src="/hackathon_landing/assets/curl1.svg" alt="Logo" height={100} width={100} />
+          <Image className='absolute rotate-180 right-2' src="/hackathon_landing/assets/curl1.svg" alt="Logo" height={150} width={150} />
           </div>
     </div>
   )
