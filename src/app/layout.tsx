dba0c3694c2bf516274532649774accb14bf43bb
Mohammed Sahal <technicalsahal25@gmail.com>
2025-02-08 22:36:35 +0530

    Errors cleared

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -17,23 +17,23 @@ export default function RootLayout({
       <link
         rel="icon"
         type="image/png"
-        href="/favicon/favicon-96x96.png"
+        href="/hackathon_landing/favicon/favicon-96x96.png"
         sizes="96x96"
       />
-      <link rel="icon" type="image/svg+xml" href="/favicon/favicon.svg" />
-      <link rel="shortcut icon" href="/favicon/favicon.ico" />
+      <link rel="icon" type="image/svg+xml" href="/hackathon_landing/favicon/favicon.svg" />
+      <link rel="shortcut icon" href="/hackathon_landing/favicon/favicon.ico" />
       <link
         rel="apple-touch-icon"
         sizes="180x180"
-        href="/favicon/apple-touch-icon.png"
+        href="/hackathon_landing/favicon/apple-touch-icon.png"
       />
-      <link rel="manifest" href="/favicon/site.webmanifest" />
+      <link rel="manifest" href="/hackathon_landing/favicon/site.webmanifest" />
       <link rel="preconnect" href="https://fonts.googleapis.com" />
       <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="" />
       <link href="https://fonts.googleapis.com/css2?family=Cormorant+Garamond:ital,wght@0,300;0,400;0,500;0,600;0,700;1,300;1,400;1,500;1,600;1,700&family=Lobster+Two:ital,wght@0,400;0,700;1,400;1,700&display=swap" rel="stylesheet"></link>
       </head>
       <body
-        className="bg-secondary bg-[url('/natural-paper.png')] bg-repeat text-gray-900 
+        className="bg-secondary bg-[url('/hackathon_landing/natural-paper.png')] bg-repeat text-gray-900 
         before:content-[''] before:absolute before:inset-0 
         before:bg-[radial-gradient(circle,rgba(0,0,0,0.15)_10%,transparent_50%),radial-gradient(circle_at_20%_80%,rgba(0,0,0,0.1)_10%,transparent_50%),radial-gradient(circle_at_80%_20%,rgba(0,0,0,0.1)_10%,transparent_50%)] 
         before:pointer-events-none before:z-10 antialiased"
